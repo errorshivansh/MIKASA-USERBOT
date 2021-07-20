76555d5f848e2dc1939f3ebd891db6f8222865bd
TEAM-MIKASA <87220607+TEAM-MIKASA@users.noreply.github.com>
2021-07-20 20:03:35 +0530

    Update alive.py

--- MikasaBot/plugins/alive.py
+++ MikasaBot/plugins/alive.py
@@ -10,7 +10,7 @@ from . import *
 #-------------------------------------------------------------------------------
 
 mikasa_pic = Config.ALIVE_PIC or "https://telegra.ph/file/ea9e11f7c9db21c1b8d5e.mp4"
-mikasa_caption = f"**🔥🔥 ℓєgєи∂яу αf мιкαѕα вσт 🔥🔥**\n\n"
+mikasa_caption = f"**__🔥🔥 ℓєgєи∂яу αf мιкαѕα вσт 🔥🔥__**\n\n"
 mikasa_caption += f"  ↼ Oᴡɴᴇʀ ⇀   : 『 {mikasa_mention} 』\n\n"
 mikasa_caption += "✘ Aʙᴏᴜᴛ Mʏ Sʏsᴛᴇᴍ ✘\n\n"
 mikasa_caption += f"🔹 Tᴇʟᴇᴛʜᴏɴ               :  `{tel_ver}` \n"
