207c4cc72a6869cf6ac7325833b8a61ab24ac9e1
TEAM-MIKASA <87220607+TEAM-MIKASA@users.noreply.github.com>
2021-07-29 10:30:07 +0530

    Update _helper.py

--- MikasaBot/plugins/_helper.py
+++ MikasaBot/plugins/_helper.py
@@ -40,8 +40,8 @@ async def yardim(event):
     except:
         pass
     if tgbotusername is not None:
-        results = await event.client.inline_query(tgbotusername, "hellbot_help")
-        await results[0].click(
+        hell = await event.client.inline_query(tgbotusername, "mikasa_help")
+        await hell[0].click(
             event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True
         )
         await event.delete()
@@ -51,7 +51,7 @@ async def yardim(event):
 
 @bot.on(mikasa_cmd(pattern="plinfo(?: |$)(.*)", outgoing=True))
 @bot.on(sudo_cmd(pattern="plinfo(?: |$)(.*)", allow_sudo=True))
-async def mikasabott(event):
+async def mikasabot(event):
     if event.fwd_from:
         return
     args = event.pattern_match.group(1).lower()
